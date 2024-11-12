03ef5262f1f82c4eb897e7b64a5dceffcb8ea84d
ibmicetests <ibmiceinfo@gmail.com>
2024-11-12 17:00:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,38 +7,30 @@ const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-// Serve static files from root directory instead of public folder
-app.use(express.static(__dirname));
-
-// Serve index.html for the root route
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
+// Serve static files
+app.use(express.static('public'));
 
 // Store connected clients
 const clients = new Set();
 
 wss.on('connection', (ws) => {
-    console.log('New client connected');
-    clients.add(ws);
-    
-    ws.on('message', (message) => {
-        console.log('Received:', message.toString());
-        // Broadcast message to all connected clients
-        clients.forEach((client) => {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(message.toString());
-            }
-        });
+  clients.add(ws);
+  
+  ws.on('message', (message) => {
+    // Broadcast message to all connected clients
+    clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message.toString());
+      }
     });
-    
-    ws.on('close', () => {
-        console.log('Client disconnected');
-        clients.delete(ws);
-    });
+  });
+  
+  ws.on('close', () => {
+    clients.delete(ws);
+  });
 });
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`);
+}); 
